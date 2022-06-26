ef90edaa5b58b8c37f2ea13b1b8567256f28cb4d
intondiscott <intondiscott@gmail.com>
2022-06-26 16:11:53 -0400

    Uploading Week 8 work

--- Readme.md
+++ Readme.md
@@ -1,2 +1,1 @@
-<h1>Initializing Repository</h1>
-<p>This is a test...</p>
+<body></body>
---
+++ gameauth/gameauth/FIXME/gameauth/src/main/java/com/gamingroom/gameauth/auth/GameAuthorizer.java
@@ -0,0 +1,16 @@
+package com.gamingroom.gameauth.auth;
+
+import io.dropwizard.auth.Authorizer;
+
+ 
+
+
+public class GameAuthorizer implements Authorizer<GameUser> 
+{
+    @Override
+    public boolean authorize(GameUser user, String role) {
+    	
+        // FIXME: Finish the authorize method based on BasicAuth Security Example
+    	return user.getRoles() != null && user.getRoles().contains(role);
+    }
+}
